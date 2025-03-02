59fae245abbb6d749c02d782d51d329979c77996
Mahmoud Ahmed <maahmed486@gmail.com>
2025-03-02 12:50:15 +0200

    logIn & SinUp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:motwakel/main.dart';
+import 'package:motwakel/presentation/log_in/LogIn.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
